633f9df6ee8ae1e9f3e44dcf46d277b213ca7672
Jonathan Chetboun <jchetboun@lightricks.com>
2019-09-10 14:50:48 +0300

    Remove prerun.

--- prerun.sh
+++
@@ -1,2 +0,0 @@
-ln -s /skydive/Datasets/Makeup_Paired_Reconstruction_Iteration/train_inst_1 /skydive/Datasets/Makeup_Paired_Reconstruction_Iteration/train_inst
-ln -s /skydive/Datasets/Makeup_Paired_Reconstruction_Iteration/test_inst_1 /skydive/Datasets/Makeup_Paired_Reconstruction_Iteration/test_inst
