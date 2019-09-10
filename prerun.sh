48ef9cbcb1d90e33897a9ffbec930b313e9afa0c
Jonathan Chetboun <jchetboun@lightricks.com>
2019-09-10 14:38:17 +0300

    Prerun test1.

--- prerun.sh
+++ prerun.sh
@@ -1,1 +1,2 @@
-python -m visdom.server
+ln -s /skydive/Datasets/Makeup_Paired_Reconstruction_Iteration/train_inst_1 /skydive/Datasets/Makeup_Paired_Reconstruction_Iteration/train_inst
+ln -s /skydive/Datasets/Makeup_Paired_Reconstruction_Iteration/test_inst_1 /skydive/Datasets/Makeup_Paired_Reconstruction_Iteration/test_inst
